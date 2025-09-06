f5086f9fbe00b9526c0759ebc1edf0e0bb27229a
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-06 21:00:19 +0600

    add border remove and add functionality

--- script/script.js
+++ script/script.js
@@ -6,8 +6,9 @@ const loadNavCategoryData = () => {
     fetch(url)
     .then(res => res.json())
     .then(data =>{
-        console.log(data.categories)
+        
         const storeCategoryData = data.categories
+        console.log(storeCategoryData)
         displayNavCategoryData(storeCategoryData)
     })
     .catch(err => console.log('There is some error here to fecth data'))
@@ -16,9 +17,22 @@ const loadNavCategoryData = () => {
 const displayNavCategoryData = (navItems) =>{
     navItems.forEach(data => {
             navLink.innerHTML += `
-                <li class="hover:border-b-4 hover:border-red-600 cursor-pointer">${data.title}</li>
+                <li id ="${data.id}" class="hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${data.title}</li>
             `
         });
+
+        navLink.addEventListener('click',(e) =>{
+
+            const li = document.querySelectorAll('li')
+            li.forEach(li =>{
+                li.classList.remove('border-b-4')
+            })
+
+            if(e.target.localName = "li"){
+                console.log(e)
+                e.target.classList.add('border-b-4')
+            }
+        })
 }
 
 loadNavCategoryData()
