45ad0981576355161b8715188d6d9bf5b7f2a6ef
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-06 19:57:46 +0600

    fetch title from api for nav link and show it in dom

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
-console.log('hello from js')
+const navLink = document.getElementById('nav-link')
+
+const loadNavCategoryData = () => {
+    const url = `https://news-api-fs.vercel.app/api/categories`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data.categories)
+        const storeCategoryData = data.categories
+        storeCategoryData.forEach(data => {
+            navLink.innerHTML += `
+                <li class="hover:border-b-4 hover:border-red-600 cursor-pointer">${data.title}</li>
+            `
+        });
+    })
+    .catch(err => console.log('There is some error here to fecth data'))
+}
+
+const displayNavCategoryData = (navItems) =>{
+
+}
+
+loadNavCategoryData()
