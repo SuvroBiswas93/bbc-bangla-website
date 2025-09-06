f4a99c4279f3103dba2c3916be21545af881625e
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-07 00:14:32 +0600

    implement show news functionality active button functionality and design card

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
-const navLink = document.getElementById('nav-link')
+const navLink = document.getElementById('nav-link');
+
+const newsContainer = document.getElementById('news-container')
 
 const loadNavCategoryData = () => {
     const url = `https://news-api-fs.vercel.app/api/categories`;
@@ -8,7 +10,7 @@ const loadNavCategoryData = () => {
     .then(data =>{
         
         const storeCategoryData = data.categories
-        console.log(storeCategoryData)
+        // console.log(storeCategoryData)
         displayNavCategoryData(storeCategoryData)
     })
     .catch(err => console.log('There is some error here to fecth data'))
@@ -17,22 +19,66 @@ const loadNavCategoryData = () => {
 const displayNavCategoryData = (navItems) =>{
     navItems.forEach(data => {
             navLink.innerHTML += `
-                <li id ="${data.id}" class="hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${data.title}</li>
+                <li id ="${data.id}" 
+                class="hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer ${data.id === "main" ? "border-b-4 border-red-600" : ""}" >
+                    ${data.title}
+                 </li>
             `
         });
 
         navLink.addEventListener('click',(e) =>{
 
-            const li = document.querySelectorAll('li')
-            li.forEach(li =>{
+            const allLi = document.querySelectorAll('li')
+            allLi.forEach(li =>{
                 li.classList.remove('border-b-4')
             })
 
             if(e.target.localName === "li"){
-                console.log(e)
+                loadNewsBycategory(e.target.id)
+
                 e.target.classList.add('border-b-4')
+
+                
             }
         })
 }
 
+
+const loadNewsBycategory = (categoryId)=>{
+    // console.log(categoryId)
+    const url = `https://news-api-fs.vercel.app/api/categories/${categoryId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        const showNews = data.articles
+        displayNewsByCategory(showNews)
+        console.log(showNews)
+    })
+    .catch(err => console.log('Data fetching error..'))
+}
+
+
+const displayNewsByCategory = (newsCard) =>{
+
+    newsContainer.innerHTML = ""
+
+    newsCard.forEach(news =>{
+        // console.log(news.image.srcset[5])
+        newsContainer.innerHTML += `
+            <div  class = "space-y-2 border border-gray-300 rounded-lg ">
+                <div>
+                    <img src="${news.image.srcset[5].url}" alt="" class ="rounded-t">
+                </div>
+                <div class = "px-3">
+                    <h1>${news.title}</h1>
+                    <p>${news.time}</p>
+                </div>
+
+           </div>
+        `
+    })
+    
+}
+
 loadNavCategoryData()
+loadNewsBycategory('main')
